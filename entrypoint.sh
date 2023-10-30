bccda3179187d5c08cde048b90c0a57ab2c8a1b7
gardais <147820580+gardais@users.noreply.github.com>
2023-10-30 10:34:08 +0000

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Default values:
+: "${INPUT_REPORTER:='terminal'}"
+: "${INPUT_PATH:=$1}"
+
+# Diagnostic output:
+echo "Using reporter: $INPUT_REPORTER"
+echo "Linting path: $INPUT_PATH"
+echo 'flake8 --version:'
+flake8 --version
+echo '================================='
+echo
+
+# Runs `flake8`, possibly with `reviewdog`:
+if [ "$INPUT_REPORTER" == 'terminal' ]; then
+  output=$(flake8 --exit-zero $INPUT_PATH)
+  status="$?"
+elif [ "$INPUT_REPORTER" == 'github-pr-review' ] ||
+     [ "$INPUT_REPORTER" == 'github-pr-check' ]; then
+  # We will need this token for `reviewdog` to work:
+  export REVIEWDOG_GITHUB_API_TOKEN="$GITHUB_TOKEN"
+  # Running special version of `flake8` to match the `reviewdog` format:
+  output=$(flake8 --exit-zero $INPUT_PATH --append-config='/action-config.cfg')
+  echo "$output" | reviewdog -f=pep8 -reporter="$INPUT_REPORTER" -level=error
+  # `reviewdog` does not fail with any status code, so we have to get dirty:
+  status=$(test "$output" = ''; echo $?)
+else
+  output="Invalid action reporter specified: $INPUT_REPORTER"
+  status=1
+fi
+echo '================================'
+
+# Fail the build in case status code is not 0:
+if [ "$status" != 0 ]; then
+  echo "Process failed with the status code: $status"
+  exit "$status"
+fi
